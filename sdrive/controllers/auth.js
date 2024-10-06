f406f39d41deff981a86dd6c68103a1a0143b78d
eRz3T <98267623+eRz3T@users.noreply.github.com>
2024-10-06 15:12:06 +0200

    Update
    - Token na ciasteczku
    - Logika logowania

--- sdrive/controllers/auth.js
+++ sdrive/controllers/auth.js
@@ -5,7 +5,7 @@ const logout = require("./logout")
 const router = express.Router();
 
 router.post("/register", register)
-//router.post("/login", login)
+router.post("/login", login)
 //router.post("/logout", logout)
 
 module.exports = router;
--- sdrive/controllers/register.js
+++ sdrive/controllers/register.js
@@ -2,6 +2,20 @@ const db = require("../routes/db-config");
 const bcrypt = require("bcryptjs");
 
 const register = async (req,res) => {
-
+    const { email, password: Npassword } = req.body
+    if (!email || !Npassword) return res.json({status: "error", error: "Podaj swój email i hasło"});
+    else {
+        db.querry('SELECT email FROM users WHERE email = ?', [email], async (err, result) => {
+            if(err) throw err;
+            if(result[0]) return res.json ({ status: "error", error: "Email już jest w systemie"})
+            else {
+                const password = bcrypt.hash(Npassword, 8);
+                db.querry ('INSERT INTO users SET ?', {email: email, password: password}, (error, results) => {
+                    if (error) throw error;
+                    return res.json({status: "success", success: "Użytkownik został zarejestrowany"})
+                })
+            }
+        })
+    }
 }
 module.exports = register;

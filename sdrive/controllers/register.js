e2e412f54f85b2d4571750eb5213cad88f98ad10
eRz3T <98267623+eRz3T@users.noreply.github.com>
2024-10-09 23:46:07 +0200

    Nowa szata graficzna
    - Szybka implementacja bootstrap do wszystkich widoków .ejs

--- sdrive/controllers/register.js
+++ sdrive/controllers/register.js
@@ -2,12 +2,10 @@ const db = require("../routes/db-config");
 const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-    const { email, password: Npassword } = req.body;
+    const { email, password: Npassword, firstName, lastName, birthDate } = req.body;
 
-    console.log("Odebrane dane:", req.body);
-
-    if (!email || !Npassword) {
-        return res.json({ status: "error", error: "Podaj swój email i hasło" });
+    if (!email || !Npassword || !firstName || !lastName || !birthDate) {
+        return res.json({ status: "error", error: "Proszę wypełnić wszystkie pola" });
     } else {
         db.query('SELECT email_users FROM users WHERE email_users = ?', [email], async (err, result) => {
             if (err) throw err;
@@ -15,7 +13,13 @@ const register = async (req, res) => {
                 return res.json({ status: "error", error: "Email już jest w systemie" });
             } else {
                 const password = await bcrypt.hash(Npassword, 8);
-                db.query('INSERT INTO users SET ?', { email_users: email, password_users: password }, (error, results) => {
+                db.query('INSERT INTO users SET ?', { 
+                    email_users: email, 
+                    password_users: password,
+                    firstname_users: firstName,
+                    lastname_users: lastName,
+                    dateofbirth_users: birthDate
+                }, (error, results) => {
                     if (error) throw error;
                     return res.json({ status: "success", success: "Użytkownik został zarejestrowany" });
                 });
--- sdrive/public/js/register.js
+++ sdrive/public/js/register.js
@@ -3,9 +3,13 @@ const form = document.getElementById("registerForm");
 form.addEventListener("submit", (event) => {
     event.preventDefault(); 
     const register = {
-        email: document.getElementById("email").value,  
-        password: document.getElementById("password").value  
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        birthDate: document.getElementById("birthDate").value
     };
+
     fetch("/api/register", {
         method: "POST",
         body: JSON.stringify(register),
